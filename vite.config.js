52ee3631a7ed559d9d6e94ceeeaf5da731badc44
PatrickLaVan <patricklavan12@gmail.com>
2024-09-23 15:37:14 +0200

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/patricklavan.github.io/",
+  base: "/vite-deploy/",
   plugins: [react()],
   assetsInclude: ['**/*.glb']
 } )
